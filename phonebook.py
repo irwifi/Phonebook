ff2bf22d023beaba5a518361fb84ef445fb775a1
Raju Malla <irwifi@gmail.com>
2015-12-25 12:46:33 +0545

    Added the navigation buttons.

--- phonebook.py
+++ phonebook.py
@@ -1,32 +1,38 @@
-from tkinter import Tk, Frame, BOTH
+import io
+import base64
+import tkinter as tk
 
-class App(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent, background="white")
+root = tk.Tk()
+root.title("Phonebook")
 
-        self.parent = parent
-        self.parent.title("Phonebook")
-        self.pack(fill=BOTH, expand=1)
-        self.init_window()
+# a little more than width and height of image
+w = 530
+h = 580
 
+sw = root.winfo_screenwidth()
+sh = root.winfo_screenheight()
 
-    def init_window(self):
-        w = 480
-        h = 520
+x = (sw - w)/2
+y = (sh - h)/2 - 40
 
-        sw = self.parent.winfo_screenwidth()
-        sh = self.parent.winfo_screenheight()
+# use width x height + x_offset + y_offset (no spaces!)
+root.geometry("%dx%d+%d+%d" % (w, h, x, y))
 
-        x = (sw - w)/2
-        y = (sh - h)/4
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+image1 = tk.PhotoImage(file = 'images/contact_add.gif')
+image2 = tk.PhotoImage(file = 'images/contact_search.gif')
+image3 = tk.PhotoImage(file = 'images/contact_list.gif')
+image4 = tk.PhotoImage(file = 'images/contact_group.gif')
+image5 = tk.PhotoImage(file = 'images/contact_favorite.gif')
 
-def main():
-    root = Tk()
-    app = App(root)
-    root.mainloop()
+# create a white canvas
+cv = tk.Canvas(bg='white')
+cv.pack(side='top', fill='both', expand='yes')
 
-if __name__ == '__main__':
-    main()
-# w = Label(root, text="Hello Tkinter!")
-# w.pack()
+# put the image on the canvas with
+# create_image(xpos, ypos, image, anchor)
+cv.create_image(5, 5, image=image1, anchor='nw')
+cv.create_image(110, 5, image=image2, anchor='nw')
+cv.create_image(215, 5, image=image3, anchor='nw')
+cv.create_image(320, 5, image=image4, anchor='nw')
+cv.create_image(425, 5, image=image5, anchor='nw')
+root.mainloop()
